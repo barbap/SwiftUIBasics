c02e7ab43a5e2087277e59a4c48145b64cd06f33
barbap <barbaraaraujopaim99@gmail.com>
2021-03-25 14:41:08 -0300

    Initial Commit

---
+++ Shared/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  Shared
+//
+//  Created by Bárbara Araújo Paim  on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
